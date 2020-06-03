0c52d3fdb0ac8677406ba00083524b3e95d12309
Ramiro Calderon <racalder@umich.edu>
2020-06-03 15:29:33 -0400

    Deploy for Responsive font size

--- dist/js/chunk-2d210c47.3fc9dfe4.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d210c47"],{b8fa:function(n,t,c){"use strict";c.r(t);var e=function(){var n=this,t=n.$createElement,c=n._self._c||t;return c("div",{staticClass:"contact"})},a=[],s=c("2877"),l={},u=Object(s["a"])(l,e,a,!1,null,null,null);t["default"]=u.exports}}]);
-//# sourceMappingURL=chunk-2d210c47.3fc9dfe4.js.map
